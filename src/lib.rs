fac8a9dc481e17c5dd1e3e4a7cdbf4fb72a60584
Chetan Jangir <chetanrjj2002@gmail.com>
2025-11-02 18:10:39 +0530

    feat: add func to ignore hidden files

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
 use std::path::PathBuf;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 pub fn search(path: &PathBuf) {
-    for entry in WalkDir::new(path).into_iter().filter_map(Result::ok) {
+    for entry in WalkDir::new(path)
+        .into_iter()
+        .filter_entry(|e| !is_hidden(e))
+        .filter_map(Result::ok)
+    {
         let entry = entry.path();
         println!("{}", entry.display())
     }
+}
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
 }
